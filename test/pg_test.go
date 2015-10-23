1213f93a6e6b121aa3454985d693021eb313d77a
Andrew Heuermann <aheuermann@gmail.com>
2015-10-23 16:44:00 -0500

    Wait for postgres image to start

--- test/pg_test.go
+++ test/pg_test.go
@@ -3,12 +3,14 @@ package test
 import (
 	"database/sql"
 	"testing"
+	"time"
 
 	_ "github.com/lib/pq"
 )
 
 func TestDB(t *testing.T) {
-	db, err := sql.Open("postgres", "postgres://postgres@postgres/pg_test?sslmode=disable")
+	time.Sleep(5 * time.Second)
+	db, err := sql.Open("postgres", "postgres://postgres@postgres/postgres?sslmode=disable")
 	if err != nil {
 		println(err.Error())
 		t.FailNow()
